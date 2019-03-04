e02f3cbcb4ed6b1eb48fdcd57217cb5b9bfb2fd8
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2019-03-04 12:50:59 +0000

    Create fil.js

---
+++ fil.js
@@ -0,0 +1,2 @@
+<div style="width: 100%; height: 90px; max-width: 728px; background: #ddd; text-align: center; display: inline-block; color: #fff; font-weight: 400; font-size: 1.2rem; white-space: nowrap; overflow: hidden; line-height: 90px; background: linear-gradient(20deg, #c40f79 0%, #7ebfff 100%);">
+اعلان ادسنس متجاوب</div>
